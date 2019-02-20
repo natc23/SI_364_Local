819146e0736a28bf8988b1448f8772a19efbdc32
Natalie <nataliecieslak@yahoo.com>
2019-02-19 23:14:55 -0500

    autos

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += [path('', include('home.urls'))]
+urlpatterns += [path('', include('home.urls')),
+                path('accounts/', include('django.contrib.auth.urls')),
+                path('autos/', include('autos.urls')),]
--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('hello/', TemplateView.as_view(template_name= 'home.html'), name='home'),]
+    path('hello/', TemplateView.as_view(template_name= 'home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='main.html')),]
