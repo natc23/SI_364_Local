e72c98bef29ee9d6cdb1b9c120152ffc007293d2
Natalie <nataliecieslak@yahoo.com>
2019-04-11 21:17:48 -0400

    adlist #2

---
+++ adlist/adlist/github_settings.py
@@ -0,0 +1,25 @@
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App
+
+# Using ngrok is hard because the url changes every time you start ngrok
+
+# If you are running on localhost, here are some settings:
+
+# Application name: Django on Desktop
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
+# Using PythonAnywhere here are some settings:
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = '6c60d0e7a5f270fdd6f9'
+SOCIAL_AUTH_GITHUB_SECRET = '34fbff49712c703a5a30417ad948111f03f30d9c'
--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -1,30 +1,175 @@
 # Create your views here.
-from ads.models import Ad
+from ads.models import Ad, Comment, Fav
+from ads.forms import CommentForm, CreateForm
 
 from django.views import View
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from ads.forms import CreateForm
+
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
 
 from ads.util import AdlistListView, AdlistDetailView, AdlistCreateView, AdlistUpdateView, AdlistDeleteView
 
 class AdListView(AdlistListView):
     model = Ad
     template_name = "ad_list.html"
+    def get(self, request) :
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'ad_list' : ad_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
 
 class AdDetailView(AdlistDetailView):
     model = Ad
     template_name = "ad_detail.html"
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
-class AdCreateView(AdlistCreateView):
-    model = Ad
-    fields = ['title', 'price', 'text']
-    template_name = "ad_form.html"
+# class AdCreateView(AdlistCreateView):
+#     model = Ad
+#     fields = ['title', 'price', 'text']
+#     template_name = "ad_form.html"
+#     success_url = reverse_lazy('ads')
+#     def get(self, request, pk=None) :
+#         form = CreateForm(instance=ad)
+#         ctx = { 'form': form }
+#         return render(request, self.template, ctx)
+#
+#     def post(self, request, pk=None) :
+#         form = CreateForm(request.POST, request.FILES or None)
+#
+#         if not form.is_valid() :
+#             ctx = {'form' : form}
+#             return render(request, self.template, ctx)
+#
+#         pic = form.save(commit=False)
+#         pic.owner = self.request.user
+#         pic.save()
+#         return redirect(self.success_url)
+class AdFormView(LoginRequiredMixin, View):
+    template = "ad_form.html"
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        if not pk :
+            form = CreateForm()
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        if not pk:
+            form = CreateForm(request.POST, request.FILES or None)
+        else:
+            pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+            form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Adjust the model owner before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
 
 class AdUpdateView(AdlistUpdateView):
     model = Ad
     fields = ['title', 'price', 'text']
     template_name = "ad_form.html"
 
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk) :
+        pic = get_object_or_404(Pic, id=pk, owner=self.request.user)
+        form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        pic = get_object_or_404(Pic, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        pic.save()
+        return redirect(self.success_url)
+
 class AdDeleteView(AdlistDeleteView):
     model = Ad
     template_name = "ad_delete.html"
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment_form = CommentForm(request.POST)
+
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse_lazy('ad_detail', args=[pk]))
+
+class CommentDeleteView(AdlistDeleteView):
+    model = Comment
+    template_name = "comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse_lazy('ad_detail', args=[ad.id])
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Add PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        fav = Fav(user=request.user, ad=t)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Delete PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, ad=t).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
+
+
+
+
+def stream_file(request, pk) :
+    pic = get_object_or_404(Pic, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = pic.content_type
+    response['Content-Length'] = len(pic.picture)
+    response.write(pic.picture)
+    return response
